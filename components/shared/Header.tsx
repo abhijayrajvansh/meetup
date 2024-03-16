cafac410c065394e3a3f0fcfef543e3ffea0fb9f
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-03-16 10:27:13 +0530

    update: audit push from local env

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -7,26 +7,33 @@ const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
+        
         <Link href={'/'} className="w-36">
           <h1 className="text-3xl sm:font-semibold text-red-500 font-bold">meetup!</h1>
         </Link>
 
-        <div className="flex w-32 justify-end gap-3">
-
+        
+        {/* navigationbar */}
         <SignedIn>
-          <UserButton afterSignOutUrl="/" />
           <Navigationbar />
         </SignedIn>
 
-        <SignedOut>
-          <Button asChild size='lg' variant='destructive'>
-            <Link href="/signin">
-              Log in
-            </Link>
-          </Button>
-        </SignedOut>
 
 
+        {/* user profile and login/logout */}
+        <div className="flex items-center w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <Navigationbar />
+          </SignedIn>
+
+          <SignedOut>
+            <Button asChild size='lg' variant='destructive'>
+              <Link href="/signin">
+                Log in
+              </Link>
+            </Button>
+          </SignedOut>
         </div>
 
       </div>
